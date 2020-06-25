4e7c69fd256062f80cf2a30c0eb7cdad98fcbdf0
Erin Soggs <erinasoggs@gmail.com>
2020-06-25 15:49:02 -0400

    made minor changes

--- index.js
+++ index.js
@@ -28,182 +28,183 @@ let usedCellArray = [];
 let gameMode = '';
 
 function capitalize(string) {
-    if (string.length <= 1) {
-        string = string.toUpperCase();
-        return string;
-    }
-    string = string[0].toUpperCase() + string[1].toLowerCase();
+  if (string.length <= 1) {
+    string = string.toUpperCase();
     return string;
+  }
+  string = string[0].toUpperCase() + string[1].toLowerCase();
+  return string;
 }
 
 // assigning individual cells on your game board
 // possible clicks
 let cellArray = [
-    cellOne,
-    cellTwo,
-    cellThree,
-    cellFour,
-    cellFive,
-    cellSix,
-    cellSeven,
-    cellEight,
-    cellNine
+  cellOne,
+  cellTwo,
+  cellThree,
+  cellFour,
+  cellFive,
+  cellSix,
+  cellSeven,
+  cellEight,
+  cellNine
 ]
 
 // all the possible combinations to win the game
 let WINNING_COMBOS = {
-    rowOne: [cellOne, cellTwo, cellThree],
-    rowTwo: [cellFour, cellFive, cellSix],
-    rowThree: [cellSeven, cellEight, cellNine],
-    columnOne: [cellOne, cellFour, cellSeven],
-    columnTwo: [cellTwo, cellFive, cellEight],
-    columnThree: [cellThree, cellSix, cellNine],
-    forwardSlash: [cellThree, cellFive, cellSeven],
-    backSlash: [cellOne, cellFive, cellNine]
+  rowOne: [cellOne, cellTwo, cellThree],
+  rowTwo: [cellFour, cellFive, cellSix],
+  rowThree: [cellSeven, cellEight, cellNine],
+  columnOne: [cellOne, cellFour, cellSeven],
+  columnTwo: [cellTwo, cellFive, cellEight],
+  columnThree: [cellThree, cellSix, cellNine],
+  forwardSlash: [cellThree, cellFive, cellSeven],
+  backSlash: [cellOne, cellFive, cellNine]
 }
 
 twoPlayer.addEventListener('click', () => {
-    twoPlayer.disabled = true;
-    onePlayer.disabled = true;
-    startBtn.disabled = false;
-    gameMode = 'twoPlayer';
+  twoPlayer.disabled = true;
+  onePlayer.disabled = true;
+  startBtn.disabled = false;
+  gameMode = 'twoPlayer';
 })
 
 onePlayer.addEventListener('click', () => {
-    twoPlayer.disabled = true;
-    onePlayer.disabled = true;
-    startBtn.disabled = false;
-    gameMode = 'onePlayer'
+  twoPlayer.disabled = true;
+  onePlayer.disabled = true;
+  startBtn.disabled = false;
+  gameMode = 'onePlayer'
 })
 
 // function to add to the unclicked cell 
 function clicked(event) {
-    event.target.textContent = currentPlayer;
-    // currentPlayer === 'x' ? 'o' : 'x';
-    event.target.removeEventListener('click', clicked);
-    event.target.addEventListener('click', alreadyClicked);
-    usedCellArray.push(event);
-    cellArray.splice(cellArray.indexOf(event), 1);
-    checkWin();
+  event.target.textContent = currentPlayer;
+  // currentPlayer === 'x' ? 'o' : 'x';
+  event.target.removeEventListener('click', clicked);
+  event.target.addEventListener('click', alreadyClicked);
+  usedCellArray.push(event);
+  cellArray.splice(cellArray.indexOf(event), 1);
+  checkWin();
 
 
-    let win = checkWin();
+  let win = checkWin();
 
-    if (win) {
-        if (currentPlayer === 'x') {
-            status.textContent = `${capitalize(playerOne)} wins!`
-        } else {
-            status.textContent = `${capitalize(playerTwo)} wins!`
-        }
-        cells.forEach((cell) => {
-            cell.removeEventListener('click', clicked);
-            cell.removeEventListener('click', alreadyClicked)
-        })
-        clearInterval(interval);
-        clockCount = 0;
-        startBtn.disabled = true;
-        onePlayer.disabled = false;
-        twoPlayer.disabled = false;
+  if (win) {
+    if (currentPlayer === 'x') {
+      status.textContent = `${capitalize(playerOne)} wins!`
     } else {
-        if (currentPlayer === 'x') {
-            //switches to player o's turn
-            currentPlayer = 'o';
-            status.textContent = `It's ${capitalize(playerTwo)}'s turn!`
-        } else if (gameMode === 'onePlayer') {
-            let randomCell = cellArray[Math.floor(Math.random() * cellArray.length)];
-            console.log(Math.floor(Math.random() * cellArray.length))
-            // console.log(randomCell);
-            randomCell.click();
-            currentPlayer = 'x';
-            status.textContent = `It's ${capitalize(playerOne)}'s turn!`
-        } else {
-            currentPlayer = 'x';
-            status.textContent = `It's ${capitalize(playerOne)}'s turn!`
-        }
+      status.textContent = `${capitalize(playerTwo)} wins!`
     }
+    cells.forEach((cell) => {
+      cell.removeEventListener('click', clicked);
+      cell.removeEventListener('click', alreadyClicked)
+    })
+    clearInterval(interval);
+    clockCount = 0;
+    startBtn.disabled = true;
+    onePlayer.disabled = false;
+    twoPlayer.disabled = false;
+  } else {
+    if (currentPlayer === 'x') {
+      //switches to player o's turn
+      currentPlayer = 'o';
+      randomCell.click();
+      status.textContent = `It's ${capitalize(playerTwo)}'s turn!`
+    } else if (gameMode === 'onePlayer') {
+      let randomCell = cellArray[Math.floor(Math.random() * cellArray.length)][0];
+      console.log(Math.floor(Math.random() * cellArray.length))
+      console.log(randomCell);
+      currentPlayer = 'x';
+      status.textContent = `It's ${capitalize(playerOne)}'s turn!`
+    } else {
+      currentPlayer = 'x';
+      status.textContent = `It's ${capitalize(playerOne)}'s turn!`
+    }
+  }
 }
 // function that tells the player to select an empty cell if they've selected one that's 
 // already in use
 function alreadyClicked() {
-    status.textContent = 'Please select an empty cell.';
+  status.textContent = 'Please select an empty cell.';
 }
 
 startBtn.addEventListener('click', () => {
-    start();
-    onePlayer.disabled = true;
-    twoPlayer.disabled = true;
+  start();
+  onePlayer.disabled = true;
+  twoPlayer.disabled = true;
 })
 
 // function to remove event listener on cells after someone has won
 function stopPlay(cellArray) {
-    cellArray.forEach(function (cell) {
-        cell.removeEventListener('click', clicked)
-    })
+  cellArray.forEach(function (cell) {
+    cell.removeEventListener('click', clicked)
+  })
 }
 
 // function to check whether or not the player has won
 function checkWin() {
-    for (let combo of Object.values(WINNING_COMBOS)) {
-        if (combo[0].textContent === '') {
-        } else if (
-            combo[0].textContent === combo[1].textContent &&
-            combo[0].textContent === combo[2].textContent
-        ) {
-            markWinner(combo);
-            status.textContent = `Player ${capitalize(currentPlayer)} wins!`;
-            startBtn.disabled = false;
-            stopPlay(cellArray);
-            return true;
-        }
+  for (let combo of Object.values(WINNING_COMBOS)) {
+    if (combo[0].textContent === '') {} else if (
+      combo[0].textContent === combo[1].textContent &&
+      combo[0].textContent === combo[2].textContent
+    ) {
+      markWinner(combo);
+      status.textContent = `Player ${capitalize(currentPlayer)} wins!`;
+      startBtn.disabled = false;
+      stopPlay(cellArray);
+      return true;
     }
+  }
 }
 
 // function that highlights the winning squares 
 function markWinner(combo) {
-    for (let cell of combo) {
-        cell.style.backgroundColor = 'green';
-        cell.style.border = '3px solid red'
-    }
+  for (let cell of combo) {
+    cell.style.backgroundColor = 'green';
+    cell.style.border = '3px solid red'
+  }
 }
 
 // function that 
 function start() {
-    playerOne = (playerOneName.value === '' ? 'x' : playerOneName.value);
-    playerTwo = (playerTwoName.value === '' ? 'o' : playerTwoName.value);
-    playerOneName.disabled = true;
-    playerTwoName.disabled = true;
-    currentPlayer = playerOne;
-    startBtn.disabled = true;
-    clearBoard();
-    status.textContent = `Player ${currentPlayer}'s turn!`;
-    cells.forEach((cell) => {
-        cell.addEventListener('click', clicked)
-    })
-    interval = setInterval(() => { updateClock() }, 1000)
+  playerOne = (playerOneName.value === '' ? 'x' : playerOneName.value);
+  playerTwo = (playerTwoName.value === '' ? 'o' : playerTwoName.value);
+  playerOneName.disabled = true;
+  playerTwoName.disabled = true;
+  currentPlayer = playerOne;
+  startBtn.disabled = true;
+  clearBoard();
+  status.textContent = `Player ${currentPlayer}'s turn!`;
+  cells.forEach((cell) => {
+    cell.addEventListener('click', clicked)
+  })
+  interval = setInterval(() => {
+    updateClock()
+  }, 1000)
 }
 
 // resets the board when someone clicks the start game button
 function clearBoard() {
-    cells.forEach((cell) => {
-        cell.textContent = '';
-        cell.style.backgroundColor = 'white';
-        cell.style.border = '1px solid black';
-        usedCellArray = [];
-        cellArray = [
-            cellOne,
-            cellTwo,
-            cellThree,
-            cellFour,
-            cellFive,
-            cellSix,
-            cellSeven,
-            cellEight,
-            cellNine
-        ]
-    })
+  cells.forEach((cell) => {
+    cell.textContent = '';
+    cell.style.backgroundColor = 'white';
+    cell.style.border = '1px solid black';
+    usedCellArray = [];
+    cellArray = [
+      cellOne,
+      cellTwo,
+      cellThree,
+      cellFour,
+      cellFive,
+      cellSix,
+      cellSeven,
+      cellEight,
+      cellNine
+    ]
+  })
 }
 //our interval 
 function updateClock() {
-    clock.textContent = clockCount;
-    clockCount += 1;
+  clock.textContent = clockCount;
+  clockCount += 1;
 }
